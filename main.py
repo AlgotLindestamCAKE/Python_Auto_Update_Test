edba64ce69ec3141b7580b4ba166b02ec681afba
Algot Lindestam <algot.lindestam@ridecake.com>
2024-01-30 14:29:22 +0100

    v2

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+import os
+import time
 import metadata
 
 def main():
-    print("Current version =",metadata.PROGRAM_VERSION)
+    printout = "Installed version = "+metadata.PROGRAM_VERSION
+    print(printout)
+
+    timestamp = time.strftime("%Y-%m-%d %H%M%S")
+    filename = "logs/"+timestamp+".txt"
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    with open(filename, "w") as f:
+        f.write(printout)
+
+if __name__ == "__main__":
+    main()
